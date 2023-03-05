e2227556ae448da9ba3a7a25e317bf48f1cbd9c8
Manoj Kumar <manojkumar.pcf@gmail.com>
2023-03-05 15:28:00 +0530

    Changes: Added swagger for AccountsController

--- src/main/java/com/dws/challenge/web/AccountsController.java
+++ src/main/java/com/dws/challenge/web/AccountsController.java
@@ -2,6 +2,10 @@ package com.dws.challenge.web;
 
 import com.dws.challenge.domain.Account;
 import com.dws.challenge.service.AccountsService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +17,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/v1/accounts")
+@Api(value = "Accounts API", description = "Used for Adding new accounts and getting information of available Accounts")
 @Slf4j
 public class AccountsController {
 
@@ -23,6 +28,12 @@ public class AccountsController {
     this.accountsService = accountsService;
   }
 
+  @ApiOperation(value = "Create an account", response = ResponseEntity.class)
+  @ApiResponses(value = {
+          @ApiResponse(code = 201, message = "Account created successfully"),
+          @ApiResponse(code = 400, message = "Invalid input provided"),
+          @ApiResponse(code = 500, message = "Internal server error occurred")
+  })
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Object> createAccount(@RequestBody @Valid Account account) {
     log.info("Creating account {}", account);
@@ -32,7 +43,13 @@ public class AccountsController {
 
     return new ResponseEntity<>(HttpStatus.CREATED);
   }
-
+  @ApiOperation(value = "Get an account by account id", response = Account.class)
+  @ApiResponses(value = {
+          @ApiResponse(code = 200, message = "Successfully retrieved the account"),
+          @ApiResponse(code = 400, message = "Invalid input provided"),
+          @ApiResponse(code = 404, message = "Account not found"),
+          @ApiResponse(code = 500, message = "Internal server error occurred")
+  })
   @GetMapping(path = "/{accountId}")
   public Account getAccount(@PathVariable String accountId) {
     log.info("Retrieving account for id {}", accountId);
